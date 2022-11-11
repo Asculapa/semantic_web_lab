8ede95fd19dc071b0e8f3c8b1201c06455dcdd8b
Olexandr <nadtochiy1915@gmail.com>
2022-11-11 17:57:38 +0200

    Added output files

--- lab 1/task 3/main.py
+++ lab 1/task 3/main.py
@@ -23,8 +23,132 @@ from rdflib import URIRef, BNode, RDF, Graph, Namespace, FOAF, XSD, Literal
 graph = Graph()
 
 keid = URIRef('http://example.com/Keid')
-lives = URIRef('http://example.com/ontology/lives')
+emma = URIRef('http://example.com/Emma')
 
-graph.add((keid, lives, Literal('1516 Henry Street, Берклі, Каліфорнія 94709, США', datatype=XSD.string)))
+bachelor = URIRef('http://example.com/bachelor')
+bachelor = URIRef('http://example.com/bachelor')
+сhemist = URIRef('http://example.com/сhemist')
+biologist = URIRef('http://example.com/biologist')
+сalifornia_university = URIRef('http://example.com/сalifornia_university')
+valencia_university = URIRef('http://example.com/valencia_university')
 
-print(graph.serialize(format='ttl'))
+canada = URIRef('http://example.com/canada')
+france = URIRef('http://example.com/france')
+portugal = URIRef('http://example.com/portugal')
+italy = URIRef('http://example.com/italy')
+germany = URIRef('http://example.com/germany')
+denmark = URIRef('http://example.com/denmark')
+sweden = URIRef('http://example.com/sweden')
+
+
+address = URIRef('http://example.com/ontology/address')
+degree = URIRef('http://example.com/ontology/degree')
+speciality = URIRef('http://example.com/ontology/speciality')
+university = URIRef('http://example.com/ontology/university')
+date_of_graduation = URIRef('http://example.com/ontology/date_of_graduation')
+visited_countries = URIRef('http://example.com/ontology/visited_countries')
+
+graph.add((keid, address, Literal('1516 Henry Street, Берклі, Каліфорнія 94709, США', datatype=XSD.string)))
+graph.add((keid, degree, bachelor))
+graph.add((keid, speciality, biologist))
+graph.add((keid, university, сalifornia_university))
+graph.add((keid, date_of_graduation, Literal(2011, datatype=XSD.integer)))
+
+
+bag = BNode()
+graph.add((keid, FOAF.interest, bag))
+graph.add((bag, RDF.type, RDF.Bag))
+graph.add((bag, FOAF.interest, Literal('birds', datatype=XSD.string)))
+graph.add((bag, FOAF.interest, Literal('ecology', datatype=XSD.string)))
+graph.add((bag, FOAF.interest, Literal('photography', datatype=XSD.string)))
+graph.add((bag, FOAF.interest, Literal('travel', datatype=XSD.string)))
+graph.add((bag, FOAF.interest, Literal('environment', datatype=XSD.string)))
+
+seq = BNode()
+graph.add((keid, visited_countries, seq))
+graph.add((seq, RDF.type, RDF.Seq))
+graph.add((seq, visited_countries, canada))
+graph.add((seq, visited_countries, france))
+
+# Емма живе за адресою Carrer de la Guardia Civil 20, 46020 Valencia, Spain. Вона має
+# ступінь магістра хімії в Університеті Валенсії з 2015 року. Її сфера знань включає
+# управління відходами , токсичні відходи, забруднення повітря. Її інтереси: їзда на
+# велосипеді, музика та подорожі. Вона відвідала Португалію, Італію, Францію, Німеччину,
+# Данію та Швецію.
+# Кейд знає Емму. Вони зустрілися в Парижі в серпні 2014 року.
+
+master = URIRef('http://example.com/master')
+meeting = URIRef('http://example.com/meeting')
+
+field_of_knowledge = URIRef('http://example.com/ontology/field_of_knowledge')
+location = URIRef('http://example.com/ontology/location')
+month = URIRef('http://example.com/ontology/month')
+year = URIRef('http://example.com/ontology/year')
+persons = URIRef('http://example.com/ontology/persons')
+
+graph.add((emma, address, Literal('Carrer de la Guardia Civil 20, 46020 Valencia, Spain', datatype=XSD.string)))
+graph.add((emma, degree, master))
+graph.add((emma, speciality, сhemist))
+graph.add((emma, university, valencia_university))
+graph.add((emma, date_of_graduation, Literal(2015, datatype=XSD.integer)))
+
+bag = BNode()
+graph.add((emma, field_of_knowledge, bag))
+graph.add((bag, RDF.type, RDF.Bag))
+graph.add((bag, field_of_knowledge, Literal('waste management')))
+graph.add((bag, field_of_knowledge, Literal('toxic waste')))
+graph.add((bag, field_of_knowledge, Literal('air pollution')))
+
+bag = BNode()
+graph.add((emma, FOAF.interest, bag))
+graph.add((bag, RDF.type, RDF.Bag))
+graph.add((bag, FOAF.interest, Literal('riding', datatype=XSD.string)))
+graph.add((bag, FOAF.interest, Literal('bikes', datatype=XSD.string)))
+graph.add((bag, FOAF.interest, Literal('music', datatype=XSD.string)))
+graph.add((bag, FOAF.interest, Literal('travel', datatype=XSD.string)))
+
+seq = BNode()
+graph.add((emma, visited_countries, seq))
+graph.add((seq, RDF.type, RDF.Seq))
+graph.add((seq, visited_countries, portugal))
+graph.add((seq, visited_countries, italy))
+graph.add((seq, visited_countries, france))
+graph.add((seq, visited_countries, germany))
+graph.add((seq, visited_countries, denmark))
+graph.add((seq, visited_countries, sweden))
+
+graph.add((keid, FOAF.knows, emma))
+
+graph.add((meeting, location, france))
+graph.add((meeting, month, Literal('August')))
+graph.add((meeting, year, Literal(2014, datatype=XSD.integer)))
+
+bag = BNode()
+graph.add((meeting, persons, bag))
+graph.add((bag, RDF.type, RDF.Bag))
+graph.add((bag, persons, emma))
+graph.add((bag, persons, keid))
+
+# • використовуючі словники FOAF, RDF, XSD тощо та власні URI (наприклад,
+# створені на базі http://example.org/) створіть RDF граф за допомогою RDFLib;
+# • запишіть свій граф у файл у форматі TURTLE;
+# • перегляньте файл і відредагуйте його так, щоб Кейд також відвідував Німеччину і
+# щоб Еммі було 36 років;
+# • виведіть на консоль усі трійки графу;
+# • виведіть на консоль трійки, що стосуються лише про Емму;
+# • виведіть на консоль трійки, що містять імена людей.
+
+# • виконайте візуалізацію та сериалізацію графу у різні формати;
+
+graph.serialize(destination='turtle.txt', format='turtle')
+graph.serialize(destination='json.txt', format='json-ld')
+graph.serialize(destination='pretty.txt', format='pretty-xml')
+
+print('Subjects:')
+print(graph.subjects())
+
+print('Predicates:')
+print(graph.predicates())
+
+print('Objects:')
+print(graph.objects())
